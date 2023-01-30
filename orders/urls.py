590f07e62b4cdb52703ef679e7fd036fc8b8a9c3
aka-rabbi-inv <87089810+aka-rabbi-inv@users.noreply.github.com>
2023-01-30 07:34:49 +0600

    fix: replace url with re_path

--- orders/urls.py
+++ orders/urls.py
@@ -1,23 +1,14 @@
-from django.conf.urls import url 
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    url(
-        r'^order/$',
-        views.order_page,
-        name='order_page'
+    re_path(r"^order/$", views.order_page, name="order_page"),
+    re_path(r"^order/api/v1/orderconfirmation", views.order_product, name="order"),
+    re_path(
+        r"^order/invoice/(?P<filename>[a-zA-Z0-9_\-\. ]+)$",
+        views.invoice_pdf,
+        name="invoice",
     ),
-    url(
-        r'^order/api/v1/orderconfirmation',
-        views.order_product,
-        name="order"
-    ),  
-    url(
-        r'^order/invoice/(?P<filename>[a-zA-Z0-9_\-\. ]+)$',
-        views.invoice_pdf, 
-        name="invoice"
-        ),
     path("", views.index, name="index"),
     path("products", views.index, name="index"),
 ]
--- products/urls.py
+++ products/urls.py
@@ -1,26 +1,19 @@
-from django.conf.urls import url 
+from django.urls import re_path
+
 # from django.urls import path
 from . import views
 
 urlpatterns = [
-    url(
-        r'^api/v1/product/(?P<pk>[0-9]+)$',
-        views.get_product,
-        name='get_product'
-    ),
-    url(
-        r'^order/api/v1/products/(?P<search>[a-zA-Z]*)$',
+    re_path(r"^api/v1/product/(?P<pk>[0-9]+)$", views.get_product, name="get_product"),
+    re_path(
+        r"^order/api/v1/products/(?P<search>[a-zA-Z]*)$",
         views.get_products,
-        name='get_products'
+        name="get_products",
     ),
-    url(
-        r'^api/v1/product/$',
+    re_path(
+        r"^api/v1/product/$",
         views.get_update_product_by_code,
-        name='get_update_product_by_code'
+        name="get_update_product_by_code",
     ),
-    url(
-        r'^api/v1/product/create$',
-        views.post_product,
-        name='post_product'
-    ),
+    re_path(r"^api/v1/product/create$", views.post_product, name="post_product"),
 ]
