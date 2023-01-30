e0407d5cf4024aec8f7a4fbbbc5dc0941dc0d2b9
aka-rabbi-inv <87089810+aka-rabbi-inv@users.noreply.github.com>
2023-01-30 07:34:18 +0600

    fix: move order model to correct location

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from products.models import Product
 
-# Create your models here.
+
+class Order(models.Model):
+    products = models.ForeignKey(Product, on_delete=models.CASCADE)
+    name = models.TextField()
+    phone = models.PositiveIntegerField(blank=True, null=True)
+    email = models.EmailField(max_length=200, blank=True, null=True)
+    quantity = models.PositiveIntegerField()
+    row_status = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(blank=True, null=True)
