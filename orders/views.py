ba50d215991966a3f8cefe6bc3173902f3f93a95
aka-rabbi-inv <87089810+aka-rabbi-inv@users.noreply.github.com>
2023-01-31 21:23:10 +0600

    feat: add view for landing page

--- orders/views.py
+++ orders/views.py
@@ -10,6 +10,9 @@ from django.http import FileResponse
 from django.http import JsonResponse
 from shop_management.settings import BASE_DIR
 from django.contrib.auth.decorators import login_required, permission_required
+from rest_framework.authtoken.models import Token
+from django.shortcuts import redirect
+from django.contrib import messages
 
 
 # Create your views here.
@@ -27,16 +30,37 @@ def order_page(request):
     """
     Renders order page.
     """
-    return render(request, "orders/order.html")
+    try:
+        token = Token.objects.get(user=request.user)
+    except Token.DoesNotExist:
+        messages.add_message(
+            request,
+            messages.INFO,
+            f"User: {request.user.username} does not have a valid access token. Please contact an admin.",
+        )
+        return redirect("landing_page")
+    return render(request, "orders/order.html", {"access_token": token.key})
 
 
-# @login_required
+@login_required(login_url="landing_page")
 def index(request):
     products = Product.objects.all()
-    return render(request, "orders/index.html", {"products": products})
+    try:
+        token = Token.objects.get(user=request.user)
+    except Token.DoesNotExist:
+        messages.add_message(
+            request,
+            messages.INFO,
+            f"User: {request.user.username} does not have a valid access token. Please contact an admin.",
+        )
+        return redirect("landing_page")
+    return render(
+        request, "orders/index.html", {"products": products, "access_token": token.key}
+    )
 
 
 @api_view(["POST"])
+@login_required
 def order_product(request):
     """
     Handles the order submission.
@@ -103,3 +127,7 @@ def invoice_pdf(request, filename):
     response = FileResponse(pdf, content_type="application/pdf", filename="invoice.pdf")
 
     return response
+
+
+def landing_page(request):
+    return render(request, "orders/landing_page.html")
